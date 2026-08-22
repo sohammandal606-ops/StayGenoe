e6e68db0f15a1f591d5abbd6d6506c42768dbf1e
soham <sohammandal605@gmail.com>
2026-08-22 10:02:00 +0530

    small change

--- app.js
+++ app.js
@@ -96,8 +96,12 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
-    res.status(statusCode).render('error', { err });
+    let message = err.message;
+    if (!message) {
+        if (typeof err === 'string') message = err;
+        else message = 'Oh No, Something Went Wrong!';
+    }
+    res.status(statusCode).render('error', { err: { message, stack: err.stack } });
 });
 
 const port = process.env.PORT || 3000;
