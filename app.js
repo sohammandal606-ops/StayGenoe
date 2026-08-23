3dfdbcbcd2145414cbce33b644aa7925aff75bca
soham <sohammandal605@gmail.com>
2026-08-23 09:14:37 +0530

    small change

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         // 1. Check if user already exists with this googleId
